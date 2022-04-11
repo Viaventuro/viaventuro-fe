e7b2fede2d18365cf9cb819ef9686c1ed5710b3f
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-11 20:38:06 +0200

    Fix landing height

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,36 +38,6 @@ export default function Home({ data, preview }) {
 
       <Blocks content={data?.homePage?.content} />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Coming very soon...</h1>
-
-        <p className={styles.description}>
-          Maak binnenkort kennis met onze gloednieuwe Volkswagen California T6.1!
-        </p>
-
-        <div className={styles.grid}>
-          <a
-            href="https://www.goboony.com/campers/belgium/limburg/lommel/17034"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.card}
-          >
-            <h2>Sneak preview &rarr;</h2>
-            <p>Neem al eens een kijkje op onze GoBoony pagina.</p>
-          </a>
-
-          <a
-            href="https://www.volkswagen-commercial-vehicles.be/nl/modellen/california-6-1.html"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.card}
-          >
-            <h2>De California T6.1 &rarr;</h2>
-            <p>Meer over de California T6.1 op de website van VW.</p>
-          </a>
-        </div>
-      </main>
-
       <footer className={styles.footer}>&copy; Viaventuro {new Date().getFullYear()}</footer>
     </div>
   );
