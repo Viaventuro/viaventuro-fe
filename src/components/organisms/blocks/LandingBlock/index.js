518736833d0207b8b8d81f004f5a1a654bab8c00
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-11 18:21:14 +0200

    Fetch and render landing data

--- src/components/organisms/blocks/LandingBlock/index.js
+++ src/components/organisms/blocks/LandingBlock/index.js
@@ -1,5 +1,21 @@
-const LandingBlock = ({}) => {
-  return <>Landingblock</>;
+import Link from 'next/link';
+import { StructuredText } from 'react-datocms';
+import styles from './LandingBlock.module.scss';
+
+const LandingBlock = ({ title, body, callToAction }) => {
+  return (
+    <div className={styles['landing-block']}>
+      <h1>{title}</h1>
+      <StructuredText data={body} />
+      {callToAction.map((cta, i) => {
+        return (
+          <Link key={i} href={cta.externalLink}>
+            <a>{cta.label}</a>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default LandingBlock;
