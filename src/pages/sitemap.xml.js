55537754781f8030466faf03ef8511db10e3fc31
Seppe <seppe.alaerts21@gmail.com>
2022-04-12 16:18:59 +0200

    Fix sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,12 +1,11 @@
 const Sitemap = () => {};
 
 export const getServerSideProps = async ({ res }) => {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://viaventuro.be</loc>
-        <lastMod>${new Date().toISOString()}</lastMod>
-      </url>
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <url>
+    <loc>https://www.viaventuro.be/</loc>
+    <lastmod>2022-04-12T14:15:48+00:00</lastmod>
+    </url>
     </urlset>
   `;
 
