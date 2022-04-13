9b1d3ca22d63378496a18e0fc46260f8df570da7
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-13 13:44:25 +0200

    Generate sitemap.xml

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,26 +1,54 @@
 import { getAllPagesSlugsWithLocalesForSitemap } from '@/lib/api/sitemap';
+import { getUrl } from '@/lib/helpers/get-url';
 
 const Sitemap = () => {};
 
-function renderUrlTag(path, slugs, _updatedAt) {
-  return slugs.map((slug) => {
-    return `
-    <url>
-      <loc>${process.env.SITE_URL}${path}</loc>
-      <lastmod>${_updatedAt}</lastmod>
-    </url>
-    `;
-  });
+function renderUrlTag(locales, defaultLocale, path, slugs, lastmod) {
+  return locales
+    .map((locale) => {
+      const slugsObject = slugs.find((slugsObject) => slugsObject.locale === locale);
+
+      if (!slugsObject) {
+        return '';
+      }
+
+      return `
+      <url>
+        <loc>${getUrl(locale, defaultLocale, path, slugsObject.slug)}</loc>
+        <lastmod>${lastmod}</lastmod>
+
+        ${locales
+          .map((locale) => {
+            const alternateSlugsObject = slugs.find(
+              (alternateSlugsObject) => alternateSlugsObject.locale === locale,
+            );
+
+            if (!alternateSlugsObject) {
+              return '';
+            }
+
+            return `
+              <link
+                rel="alternate"
+                hreflang="${locale.toLowerCase()}"
+                href="${getUrl(locale, defaultLocale, path, alternateSlugsObject.slug)}"
+              />
+            `;
+          })
+          .join('')}
+        </url>`;
+    })
+    .join('');
 }
 
-export const getServerSideProps = async ({ res }) => {
+export const getServerSideProps = async ({ res, locale, locales, defaultLocale }) => {
   const pages = await getAllPagesSlugsWithLocalesForSitemap();
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
       ${pages.map((page) => {
         const { path, slugs, _updatedAt } = page;
-        return renderUrlTag(path, slugs, _updatedAt);
+        return renderUrlTag(locales, defaultLocale, path, slugs, _updatedAt);
       })}
     </urlset>
   `;
