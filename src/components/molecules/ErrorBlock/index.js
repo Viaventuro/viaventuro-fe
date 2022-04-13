d140976f9f4cc094339e869798d4b3f954372f79
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-13 22:01:11 +0200

    Add pagehead to error block

--- src/components/molecules/ErrorBlock/index.js
+++ src/components/molecules/ErrorBlock/index.js
@@ -1,24 +1,27 @@
 import { IconLogo } from '@/assets/icons';
 import Button from '@/components/atoms/Button';
 import Icon from '@/components/atoms/Icon';
+import PageHead from '@/components/atoms/PageHead';
 import Title from '@/components/atoms/Title';
-import { StructuredText } from 'react-datocms';
 import styles from './ErrorBlock.module.scss';
 
 const ErrorBlock = ({ title, subTitle, ctaLabel, ctaLink }) => {
   return (
-    <section className="vv-layout">
-      <div className={styles['error-block']}>
-        <div className={styles['error-block__logo__wrapper']}>
-          <Icon icon={IconLogo} className={styles['error-block__logo']} />
+    <>
+      <PageHead />
+      <section className="vv-layout">
+        <div className={styles['error-block']}>
+          <div className={styles['error-block__logo__wrapper']}>
+            <Icon icon={IconLogo} className={styles['error-block__logo']} />
+          </div>
+
+          <Title title={title} modUppercase modCenter />
+
+          <span className={styles['error-block__body']}>{subTitle}</span>
+          {ctaLabel && ctaLink && <Button label={ctaLabel} href={ctaLink} modLink modWithArrow />}
         </div>
-
-        <Title title={title} modUppercase modCenter />
-
-        <span className={styles['error-block__body']}>{subTitle}</span>
-        {ctaLabel && ctaLink && <Button label={ctaLabel} href={ctaLink} modLink modWithArrow />}
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
