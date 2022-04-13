ff37762a016b7ccbc8d936e4bf7f039d1ca8d35d
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-13 11:33:18 +0200

    Add google analytics tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,22 @@ const Document = ({}) => {
   return (
     <Html>
       <Head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;700;800&family=Mandali&display=swap"
-          rel="stylesheet"
-        /> */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
