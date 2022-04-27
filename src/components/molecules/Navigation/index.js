b6ad8454d7b913913e4a44b5df7fa8e5c49ab2c2
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-04-27 16:33:17 +0200

    Add navigation component

---
+++ src/components/molecules/Navigation/index.js
@@ -0,0 +1,23 @@
+import NavButton from '@/components/atoms/NavButton';
+import styles from './Navigation.module.scss';
+
+const Navigation = ({ navigation }) => {
+  return (
+    <nav>
+      <ul className={styles['navigation__list']}>
+        {navigation.map((item, i) => {
+          return (
+            <li key={i} className={styles['navigation__list__item']}>
+              <NavButton label={item.label} href={item.href} />
+            </li>
+          );
+        })}
+        <li className={styles['navigation__list__item']}>
+          <NavButton label="Contact" href="#contact" modCta />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
