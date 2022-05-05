974cdb881e4f0440065d580b9cf2a970e11910fa
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-05-05 15:49:20 +0200

    Add active page logic

--- src/components/molecules/Navigation/index.js
+++ src/components/molecules/Navigation/index.js
@@ -1,14 +1,21 @@
 import NavButton from '@/components/atoms/NavButton';
+import { useRouter } from 'next/router';
 import styles from './Navigation.module.scss';
 
 const Navigation = ({ navigation }) => {
+  const router = useRouter();
+
   return (
     <nav>
       <ul className={styles['navigation__list']}>
         {navigation.map((item, i) => {
           return (
             <li key={i} className={styles['navigation__list__item']}>
-              <NavButton label={item.label} slug={item.slug} modActive={i === 0} />
+              <NavButton
+                label={item.label}
+                slug={item.slug}
+                modActive={`/${item.slug}` === router.asPath}
+              />
             </li>
           );
         })}
