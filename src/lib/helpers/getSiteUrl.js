664379f4b821bf692a0f3bc3456e67652672f04d
Seppe Alaerts <seppe.alaerts@craftzing.com>
2022-05-20 15:34:05 +0200

    Revert "Remove window check"
    This reverts commit 6f802be7dd44bc984b733fe93409add43a2eca29.

--- src/lib/helpers/getSiteUrl.js
+++ src/lib/helpers/getSiteUrl.js
@@ -9,6 +9,8 @@ export default function getSiteUrl(locale) {
 
   if (process.env.SITE_URL) {
     url = process.env.SITE_URL;
+  } else if (typeof window !== 'undefined') {
+    url = `${window.location.protocol}//${window.location.hostname}`;
   } else {
     url = '';
   }
